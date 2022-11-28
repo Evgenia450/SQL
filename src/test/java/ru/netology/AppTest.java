2648488d0cb4c102def2100da0e2eb2b0cba99e0
sergey.perov <sergey.perov@soft-machine.ru>
2022-11-28 23:23:40 +0300

    update

--- src/test/java/ru/netology/AppTest.java
+++ src/test/java/ru/netology/AppTest.java
@@ -25,7 +25,6 @@ public class AppTest {
 
     @Test
     void shouldBlockedSystem() {
-        Faker faker = new Faker(new Locale("ru"));
         open("http://localhost:9999");
         var loginPage = new LoginPage();
         var authInfo = DataHelper.getInvalidAuthInfo();
